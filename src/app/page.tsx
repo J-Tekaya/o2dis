2a099ec7168a4ff7d4279621a510ffb166d08835
TEKAYA Jihen <tekayajihen@Ordinateur-portable-de-TEKAYA.local>
2024-06-21 09:55:12 +0100

    Add default metadata to layout + add header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="font-montserrat text-5xl font-bold">O2DIS</h1>
-      <h1 className="font-montserrat text-5xl">Everyone has the right to freedom</h1>
     </main>
   );
 }
